acbd43efc3343eaf8e77f9d075bcea7531dec08f
ronak-techiebutler <ronak.techiebutler@gmail.com>
2026-05-06 12:44:30 +0530

    chore: configure EAS build, update Expo scripts, and add URL polyfill support

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-url-polyfill/auto';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
